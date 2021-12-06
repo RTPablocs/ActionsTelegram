482b153cc1c14e34a7b6ecf9bdcefcda6385e03c
RTPablocs <conejerosorianopablo@gmail.com>
2021-12-06 15:47:11 +0100

    idk

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const bot = new TelegramBot(token, { polling: true });
 
 try {
     bot.sendMessage(187940793, "Action Completada!");
+    console.log('Done!');
 } catch (error) {
     core.setFailed(error.message);
 }

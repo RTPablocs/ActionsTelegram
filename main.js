62afc71619af5d7c3214a770a194b882f4d40019
RTPablocs <conejerosorianopablo@gmail.com>
2021-12-06 15:42:44 +0100

    Fix #5

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
 const core = require('@actions/core');
 const github = require('@actions/github');
+const token = core.getInput('token');
+const bot = new TelegramBot(token, { polling: true });
 
 try {
-    const token = core.getInput('token');
-    const bot = new TelegramBot(token, { polling: true });
     bot.sendMessage(187940793, "Action Completa!");
-    process.exit(1)
 } catch (error) {
     core.setFailed(error.message);
 }

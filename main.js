9ef2cd28a9ec700c999996cc11984812939289e8
RTPablocs <conejerosorianopablo@gmail.com>
2021-12-06 15:52:04 +0100

    #idk 3

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const core = require('@actions/core');
 const github = require('@actions/github');
 const token = core.getInput('token');
+console.log(token);
 const bot = new TelegramBot(token, { polling: true });
 
 
